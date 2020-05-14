78d06e295bc241af1e4f5975e5674a0a49576615
Marian Q <mariansofia1999@hotmail.com>
2020-05-14 15:19:32 -0500

    Updates

---
+++ precache-manifest.1cd863ce7b57f25b8760c2d56da9b578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93fd564d9617623b04c9ad054bdbf8d1",
+    "url": "/201-adv-blade-master/index.html"
+  },
+  {
+    "revision": "7e7fd33e0f2db9f760d9",
+    "url": "/201-adv-blade-master/static/css/main.f77e26b4.chunk.css"
+  },
+  {
+    "revision": "c492bd19d1d6db73b9c4",
+    "url": "/201-adv-blade-master/static/js/2.14ae555d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/201-adv-blade-master/static/js/2.14ae555d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7fd33e0f2db9f760d9",
+    "url": "/201-adv-blade-master/static/js/main.a55c0ce9.chunk.js"
+  },
+  {
+    "revision": "42bc1afccc06e6f14fe9",
+    "url": "/201-adv-blade-master/static/js/runtime-main.2941959f.js"
+  }
+]);
